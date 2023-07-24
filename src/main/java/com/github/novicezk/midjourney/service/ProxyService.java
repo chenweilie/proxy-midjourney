b7a51264dbb0a94eda69e0e3c420cd2001c37350
alex <alex@li.com>
2023-07-24 19:22:48 +0800

    111

--- src/main/java/com/github/novicezk/midjourney/service/ProxyService.java
+++ src/main/java/com/github/novicezk/midjourney/service/ProxyService.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import java.net.URISyntaxException;
 
 public interface ProxyService {
-    ResponseEntity<byte[]> proxyImage(String id);
+
 
     ResponseEntity<byte[]> proxy(String reqPath) throws URISyntaxException;
 }
--- src/main/java/com/github/novicezk/midjourney/service/ProxyServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/ProxyServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.novicezk.midjourney.service;
 import com.github.novicezk.midjourney.exception.InvalidSessionException;
 import com.github.novicezk.midjourney.support.Task;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,45 +17,50 @@ import java.net.URISyntaxException;
 import java.util.Collections;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ProxyServiceImpl implements ProxyService{
 
     @Autowired
     private TaskStoreService taskStoreService;
 
-    @Override
-    public ResponseEntity<byte[]> proxyImage(String id) {
-
-        Task task = taskStoreService.get(id);
-
-        if (null == task){
-            return ResponseEntity.notFound().build();
-        }
-
-        String url = task.getImageUrl();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        //有BUG，只能返回png格式的图片
-        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
-            request.getHeaders().add("Accept", "image/png");
-            return execution.execute(request, body);
-        }));
-
-        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_TYPE, "image/png");
-
-        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
-    }
+//    @Override
+//    public ResponseEntity<byte[]> proxyImage(String id) {
+//
+//        Task task = taskStoreService.get(id);
+//
+//        if (null == task){
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        String url = task.getImageUrl();
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        //有BUG，只能返回png格式的图片
+//        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+//            request.getHeaders().add("Accept", "image/png");
+//            return execution.execute(request, body);
+//        }));
+//
+//        RequestEntity<Void> request = RequestEntity.get(new URI(url)).build();
+//        ResponseEntity<byte[]> response = restTemplate.exchange(request, byte[].class);
+//        byte[] imageBytes = response.getBody();
+//
+////        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.CONTENT_TYPE, "image/png");
+//
+//        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+//    }
 
     @Override
     public ResponseEntity<byte[]> proxy(String reqPath) throws URISyntaxException {
 
         String url = "https://cdn.discordapp.com/" + reqPath;
 //        String url = "http://34.68.103.183:42146/api/cnd-discordapp/" + reqPath;
-        System.out.println(url);
+        log.info("url",url);
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -63,13 +69,13 @@ public class ProxyServiceImpl implements ProxyService{
             request.getHeaders().add("Accept", "image/png");
             return execution.execute(request, body);
         }));
-
+        log.info("准备请求图片");
         RequestEntity<Void> request = RequestEntity.get(new URI(url)).build();
         ResponseEntity<byte[]> response = restTemplate.exchange(request, byte[].class);
         byte[] imageBytes = response.getBody();
 
 //        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-
+        log.info("response",response);
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_TYPE, "image/png");
 
